b3d15c63219d065c61367b987fbc6958d8a0b75a
lyleaigbedion <Lyle-Anthony.Aigbedion75@myhunter.cuny.edu>
2020-04-27 13:34:56 -0400

    implemnted readdir in node

--- bash.js
+++ bash.js
@@ -1,5 +1,6 @@
 // Output prompt
 const pwd = require('./pwd');
+const ls = require('./ls');
 process.stdout.write('prompt > ');
 
 //stdin 'data' event fires after a user types in a line
@@ -8,12 +9,16 @@ process.stdin.on('data', (data)=> {
 
   if(cmd === 'pwd'){
     pwd();
-
+    process.stdout.write('\nprompt > ');
+  }
+  else if(cmd === 'ls'){
+    ls();
   }
   else{
     process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
   }
+  // process.stdout.write('\nprompt > ');
 
-  process.stdout.write('\nprompt > ');
 
 });
